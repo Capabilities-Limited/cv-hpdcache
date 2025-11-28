289243a9a3f62dcc12d1f6c7a1a00dc3a640b07a
Jonathan Woodruff <jdw57@cam.ac.uk>
2025-11-28 17:28:48 +0000

    Remove user fields from resizer, as this allows us to build with 64-bit external AXI accesses, as required by the testbench.

--- rtl/src/hpdcache_miss_handler.sv
+++ rtl/src/hpdcache_miss_handler.sv
@@ -658,8 +658,8 @@ import hpdcache_pkg::*;
     );
 
     hpdcache_data_resize #(
-        .WR_WIDTH (HPDcacheCfg.u.memDataWidth + HPDcacheCfg.u.memUserWidth),
-        .RD_WIDTH (HPDcacheCfg.accessWidth + HPDcacheCfg.u.accessUserWidth),
+        .WR_WIDTH (HPDcacheCfg.u.memDataWidth/* + HPDcacheCfg.u.memUserWidth*/),
+        .RD_WIDTH (HPDcacheCfg.accessWidth/* + HPDcacheCfg.u.accessUserWidth*/),
         .DEPTH    (HPDcacheCfg.u.refillFifoDepth)
     ) i_data_resize(
         .clk_i,
@@ -667,12 +667,12 @@ import hpdcache_pkg::*;
 
         .w_i    (refill_fifo_resp_data_w),
         .wok_o  (refill_fifo_resp_data_wok),
-        .wdata_i({mem_resp_i.mem_resp_r_data, mem_resp_i.mem_resp_r_user}),
+        .wdata_i({mem_resp_i.mem_resp_r_data/*, mem_resp_i.mem_resp_r_user*/}),
         .wlast_i(mem_resp_i.mem_resp_r_last),
 
         .r_i    (refill_fifo_resp_data_r),
         .rok_o  (/* unused */),
-        .rdata_o({refill_fifo_resp_data_rdata, refill_fifo_resp_data_ruser}),
+        .rdata_o({refill_fifo_resp_data_rdata/*, refill_fifo_resp_data_ruser*/}),
         .rlast_o(/* unused */)
     );
 
