f4e330e6ef5f79e3466e73d79aee108331f46c81
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2026-01-19 18:14:41 +0000

    Work in progress support for AMOs with wider req width and caps

--- rtl/src/hpdcache_amo.sv
+++ rtl/src/hpdcache_amo.sv
@@ -29,40 +29,53 @@ import hpdcache_pkg::*;
 //  Ports
 //  {{{
 (
-    input  logic [63:0]           ld_data_i,
-    input  logic [63:0]           st_data_i,
+    input  logic [128:0]          ld_data_i,
+    input  logic [128:0]          st_data_i,
     input  hpdcache_uc_op_t       op_i,
-    output logic [63:0]           result_o
+    output logic [128:0]          result_o
 );
 //  }}}
 
-    logic signed [63:0] ld_data;
-    logic signed [63:0] st_data;
+    logic signed [63:0] ld_data_signed;
+    logic signed [63:0] st_data_signed;
     logic signed [63:0] sum;
+    logic        [63:0] ld_data_unsigned;
+    logic        [63:0] st_data_unsigned;
+    logic        [63:0] result;
+    logic        [64:0] result_hi;
     logic               ugt, sgt;
 
-    assign ld_data = ld_data_i,
-           st_data = st_data_i;
+    assign ld_data_signed = ld_data_i[63:0],
+           st_data_signed = st_data_i[63:0],
+           ld_data_unsigned = ld_data_i[63:0],
+           st_data_unsigned = st_data_i[63:0];
 
-    assign ugt = (ld_data_i > st_data_i),
-           sgt = (ld_data   > st_data),
-           sum =  ld_data   + st_data;
+    assign ugt = (ld_data_unsigned > st_data_unsigned),
+           sgt = (ld_data_signed   > st_data_signed),
+           sum =  ld_data_signed   + st_data_signed;
 
     always_comb
     begin : amo_compute_comb
         unique case (1'b1)
-            op_i.is_amo_lr   : result_o = ld_data_i;
-            op_i.is_amo_sc   : result_o = st_data_i;
-            op_i.is_amo_swap : result_o = st_data_i;
-            op_i.is_amo_add  : result_o = sum;
-            op_i.is_amo_and  : result_o = ld_data_i & st_data_i;
-            op_i.is_amo_or   : result_o = ld_data_i | st_data_i;
-            op_i.is_amo_xor  : result_o = ld_data_i ^ st_data_i;
-            op_i.is_amo_max  : result_o = sgt ? ld_data_i : st_data_i;
-            op_i.is_amo_maxu : result_o = ugt ? ld_data_i : st_data_i;
-            op_i.is_amo_min  : result_o = sgt ? st_data_i : ld_data_i;
-            op_i.is_amo_minu : result_o = ugt ? st_data_i : ld_data_i;
-            default          : result_o = '0;
+            op_i.is_amo_lr   : result = ld_data_unsigned;
+            op_i.is_amo_sc   : result = st_data_unsigned;
+            op_i.is_amo_swap : result = st_data_unsigned;
+            op_i.is_amo_add  : result = sum;
+            op_i.is_amo_and  : result = ld_data_unsigned & st_data_unsigned;
+            op_i.is_amo_or   : result = ld_data_unsigned | st_data_unsigned;
+            op_i.is_amo_xor  : result = ld_data_unsigned ^ st_data_unsigned;
+            op_i.is_amo_max  : result = sgt ? ld_data_unsigned : st_data_unsigned;
+            op_i.is_amo_maxu : result = ugt ? ld_data_unsigned : st_data_unsigned;
+            op_i.is_amo_min  : result = sgt ? st_data_unsigned : ld_data_unsigned;
+            op_i.is_amo_minu : result = ugt ? st_data_unsigned : ld_data_unsigned;
+            default          : result = '0;
         endcase
+        unique case (1'b1)
+            op_i.is_amo_lr   : result_hi = ld_data_i[128:64];
+            op_i.is_amo_sc   : result_hi = st_data_i[128:64];
+            op_i.is_amo_swap : result_hi = st_data_i[128:64];
+            default          : result_hi = '0;
+        endcase
+        result_o = {result_hi, result};
     end
 endmodule
