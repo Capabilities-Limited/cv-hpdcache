28230e5eb57cba8455237fc9e155664a7eec8c75
Peter Rugg <peter.rugg@capabilitieslimited.co.uk>
2026-04-10 17:55:27 +0100

    Fix testbench build with new user bit support in hpdcache
    The built configs disable the user bits for now

--- rtl/tb/hpdcache_wrapper.sv
+++ rtl/tb/hpdcache_wrapper.sv
@@ -20,6 +20,7 @@ import hpdcache_pkg::*;
         nRequesters: (4'b1 << `CONF_HPDCACHE_REQ_SRC_ID_WIDTH),
         paWidth: `CONF_HPDCACHE_PA_WIDTH,
         wordWidth: `CONF_HPDCACHE_WORD_WIDTH,
+        wordUserWidth: `CONF_HPDCACHE_WORD_USER_WIDTH,
         sets: `CONF_HPDCACHE_SETS,
         ways: `CONF_HPDCACHE_WAYS,
         clWords: `CONF_HPDCACHE_CL_WORDS,
@@ -52,6 +53,7 @@ import hpdcache_pkg::*;
         memDataWidth: `CONF_HPDCACHE_MEM_DATA_WIDTH,
         wtEn: `CONF_HPDCACHE_WT_ENABLE,
         wbEn: `CONF_HPDCACHE_WB_ENABLE,
+        userEn: `CONF_HPDCACHE_USER_ENABLE,
         lowLatency: `CONF_HPDCACHE_LOW_LATENCY,
         eccEn: `CONF_HPDCACHE_ECC_ENABLE,
         eccScrubberEn: `CONF_HPDCACHE_ECC_SCRUBBER_ENABLE
@@ -241,7 +243,8 @@ import hpdcache_pkg::*;
     assign mem_resp_read.mem_resp_r_error = mem_resp_read_error_i,
            mem_resp_read.mem_resp_r_id    = mem_resp_read_id_i,
            mem_resp_read.mem_resp_r_data  = mem_resp_read_data_i,
-           mem_resp_read.mem_resp_r_last  = mem_resp_read_last_i;
+           mem_resp_read.mem_resp_r_last  = mem_resp_read_last_i,
+           mem_resp_read.mem_resp_r_user  = '0;
 
     assign mem_resp_read_inval = '0,
            mem_resp_read_inval_nline = '0;
